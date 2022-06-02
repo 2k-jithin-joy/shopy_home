322aa02813566ada3530c605cd26af77b6d1d677
jithin joy <jithinjoysreekovil@gmail.com>
2022-06-02 12:15:37 -0800

    1)post method implementation(login.html-->bottom of the <form> write jinjacode-->{% csrf_token %}) ,2)invalid msg printing and page display when the datas are to be entered

--- shopy_home/home/urls.py
+++ shopy_home/home/urls.py
@@ -3,9 +3,9 @@ from home import views
 
 urlpatterns = [
     path('',views.index,name="home page"),
-    path('login1/',views.log),
+    path('login1/',views.log,name="loginpage"),
     path('register/',views.reg),
     path('register/registersub/',views.registersub),
-    path('login1/loginsub/',views.loginsub),
+   
     path('logout/',views.logout)
 ]
--- shopy_home/home/views.py
+++ shopy_home/home/views.py
@@ -1,28 +1,54 @@
 from urllib import request
 from django.shortcuts import render,redirect
-from django.contrib.auth.models import auth
+from django.contrib.auth.models import auth,User
 #from django.http import HttpResponse
 
 
 def index(request):
     return render(request,'index.html')
 def log(request):
-    return render(request,'login.html')
+    if request.method=='POST':
+        user=request.POST['uname']
+        pas=request.POST['p_wname']
+        valid=auth.authenticate(username=user,password=pas)
+        if valid is not None:
+            auth.login(request,valid)
+            return redirect('/')
+        else:
+            msg="invalid user name and password"
+            return render(request,'login.html',{'lng':msg})
+    else:  
+        return render(request,'login.html')
 
 def reg(request):
     print("hello",request)
     return render(request,'register.html')
-def loginsub(request):
-    user=request.GET['uname']
-    pas=request.GET['p_wname']
-    valid=auth.authenticate(username=user,password=pas)
-    if valid is not None:
-        auth.login(request,valid)
-        return redirect('/')
-    else:
-        msg="invalid user name and password"
-        return render(request,'login.html',{'lng':msg})
+
 def registersub(request):
+    user=request.GET['uname']
+    fnm=request.GET['fname']
+    lnm=request.GET['lname']
+    eml=request.GET['email']
+    pas=request.GET['p_w']
+    rep=request.GET['re-p_w']
+    if user != '' and eml !='' and pas != '':
+        if User.objects.filter(username=user).exists() or User.objects.filter(email=eml).exists():
+            msg="username or email is already taken"
+            return render(request,'register.html',{'lng':msg})
+        else:
+            return redirect('/')
+    else:
+        msg="invalid"
+        return render(request,'register.html',{'lng':msg})
+        
+        
+    
+        
+        
+        
+    
+        
+    print(user,fnm,lnm,eml,pas)
     return render(request,'sample.html')
 def logout(request):
     auth.logout(request)
